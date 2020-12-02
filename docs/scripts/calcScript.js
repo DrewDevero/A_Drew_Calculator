d081382bf867ab0eb2159a4b8faef44731e3d548
Drew Devero <deverobelfon@gmail.com>
2020-12-02 18:46:24 -0500

    added to average button functionality

--- docs/scripts/calcScript.js
+++ docs/scripts/calcScript.js
@@ -1,11 +1,10 @@
 const NUMBERS = document.getElementsByClassName("numbers");
-console.log(NUMBERS);
 const OPERATORS = document.getElementsByClassName("operators");
-console.log(OPERATORS);
 const AVERAGE = document.getElementById("average");
 const EQUALS = document.getElementById("equals");
 const INPUT = document.getElementById("input");
 const CLEAR = document.getElementById("clearButton");
+let numToPush = "";
 let arrayOfNums = [];
 
 function buttonDefault(button) {
@@ -13,12 +12,36 @@ function buttonDefault(button) {
         e.preventDefault()
     )
 }
-function buttonPressed(pressed) {
+function numberPressed(pressed) {
     pressed.addEventListener("click", () => {
         INPUT.value += pressed.innerHTML;
-            pressed.innerHTML !== "0" ? arrayOfNums.push(parseInt(pressed.innerHTML)) : "";
+        return numToPush += pressed.innerHTML;
         }
     )
+}
+function operatorPressed(pressed) {
+    const PLACEHOLDER_INT = parseInt(INPUT.placeholder);
+        if (pressed === OPERATORS[0]) {
+            pressed.addEventListener("click", () => {
+                if(typeof PLACEHOLDER_INT === "number" && PLACEHOLDER_INT !== 0) {
+                    INPUT.value = INPUT.placeholder + pressed.innerHTML;
+                    arrayOfNums.push(PLACEHOLDER_INT);
+                    INPUT.placeholder = "0";
+                } else {
+                    INPUT.value += pressed.innerHTML;
+                    parseInt(numToPush) !== 0 ? (arrayOfNums.push(parseInt(numToPush)), numToPush = "")  : "";
+                }
+            })
+        } else {
+            pressed.addEventListener("click", () => {
+                if(typeof PLACEHOLDER_INT === "number" && PLACEHOLDER_INT !== 0) {
+                    INPUT.value = INPUT.placeholder + pressed.innerHTML;
+                    INPUT.placeholder = "0";
+                } else {
+                    INPUT.value += pressed.innerHTML;
+                }
+            })
+        }     
 }
 function equals(evaluate) {
     evaluate.addEventListener("click", () =>
@@ -27,14 +50,16 @@ function equals(evaluate) {
 }
 function average(evaluate) {
     evaluate.addEventListener("click", () => {
-         INPUT.value = arrayOfNums.reduce((a,b) => a + b) / arrayOfNums.length;    
+         numToPush === "" ? (INPUT.value = "", INPUT.placeholder = arrayOfNums.reduce((a,b) => a + b) / arrayOfNums.length, arrayOfNums = []) : (arrayOfNums.push(parseInt(numToPush)), INPUT.value = "", INPUT.placeholder = arrayOfNums.reduce((a,b) => a + b) / arrayOfNums.length, numToPush = "", arrayOfNums = []);
+        return numToPush, arrayOfNums;
         }
     )
 }
 function clear(clearPressed) {
     clearPressed.addEventListener("click", () => {
         INPUT.value = null;
-        arrayOfNums = [];
+        INPUT.placeholder = "0";
+        return numToPush = "", arrayOfNums = [];      
         }
     )
 }
@@ -47,9 +72,9 @@ clear(CLEAR);
 
 for(let i = 0; i < NUMBERS.length; i++) {
         buttonDefault(NUMBERS[i]);
-        buttonPressed(NUMBERS[i]);       
+        numberPressed(NUMBERS[i]);       
     }
 for(let j = 0; j < OPERATORS.length; j++) {
         buttonDefault(OPERATORS[j]);
-        buttonPressed(OPERATORS[j]);
+        operatorPressed(OPERATORS[j]);
     }
